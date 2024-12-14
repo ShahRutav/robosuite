7bebf12ff8952fb3d5f823453a6c9087a5af85f1
ShahRutav <rutavms@utexas.edu>
2024-12-14 13:18:05 +0530

    updated demo robot to work in simulation

--- robosuite/models/robots/manipulators/demo_robot.py
+++ robosuite/models/robots/manipulators/demo_robot.py
@@ -10,70 +10,6 @@ class Demo(ManipulatorModel):
     def __init__(self, idn=0):
         super().__init__(xml_path_completion("robots/demo/robot.xml"), idn=idn)
         self.set_joint_attribute(attrib="damping", values=np.array((10.0,)*6))
-        # self._key_map = {"joint": {}, "actuator": {}}
-        # self.grippers = OrderedDict()
-
-
-    # def add_gripper(self, gripper, arm_name=None):
-    #     """
-    #     Mounts @gripper to arm.
-
-    #     Throws error if robot already has a gripper or gripper type is incorrect.
-
-    #     Args:
-    #         gripper (GripperModel): gripper MJCF model
-    #         arm_name (str): name of arm mount -- defaults to self.eef_name if not specified
-
-    #     Raises:
-    #         ValueError: [Multiple grippers]
-    #     """
-    #     if arm_name is None:
-    #         arm_name = self.eef_name
-    #     if arm_name in self.grippers:
-    #         raise ValueError("Attempts to add multiple grippers to one body")
-
-    #     # self.merge(gripper, merge_body=arm_name)
-    #     # self.merge_assets(gripper)
-
-    #     # self.grippers[arm_name] = gripper
-
-    #     # # Update cameras in this model
-    #     # self.cameras = self.get_element_names(self.worldbody, "camera")
-
-    # @property
-    # def eef_name(self):
-    #     """
-    #     Returns:
-    #         str or dict of str: Prefix-adjusted eef name for this robot. If bimanual robot, returns {"left", "right"}
-    #             keyword-mapped eef names
-    #     """
-    #     return self.correct_naming(self._eef_name)
-
-    # -------------------------------------------------------------------------------------- #
-    # Public Properties: In general, these are the name-adjusted versions from the private   #
-    #                    attributes pulled from their respective raw xml files               #
-    # -------------------------------------------------------------------------------------- #
-
-    # @property
-    # def eef_name(self):
-    #     """
-    #     Returns:
-    #         str or dict of str: Prefix-adjusted eef name for this robot. If bimanual robot, returns {"left", "right"}
-    #             keyword-mapped eef names
-    #     """
-    #     return self.correct_naming(self._eef_name)
-
-    # @property
-    # def models(self):
-    #     """
-    #     Returns a list of all m(sub-)models owned by this robot model. By default, this includes the gripper model,
-    #     if specified
-
-    #     Returns:
-    #         list: models owned by this object
-    #     """
-    #     models = super().models
-    #     return models + list(self.grippers.values())
 
     # -------------------------------------------------------------------------------------- #
     # -------------------------- Private Properties ---------------------------------------- #
@@ -101,7 +37,7 @@ class Demo(ManipulatorModel):
         #         "left-eef": "left_gripper_mount",
         #         "left-tool": "left_tool_mount"}
         return {
-            "right": "right_gripper_mount",
+            "right": "right_gripper_mount", # merge name
             # "right-tool": "right_tool_mount",
             # "left": "left_gripper_mount",
             # "left-tool": "left_tool_mount"
@@ -114,8 +50,9 @@ class Demo(ManipulatorModel):
 
     @property
     def default_base(self):
-        return "NoActuationBase"
+        # return "FixedLeggedBase"
         # return "RethinkMount"
+        return "NoActuationBase"
 
     @property
     def default_gripper(self):
@@ -141,9 +78,12 @@ class Demo(ManipulatorModel):
     def base_xpos_offset(self):
         # Copied from panda.py
         return {
-            "bins": (-0.5, -0.1, 0),
-            "empty": (-0.6, 0, 0),
-            "table": lambda table_length: (-0.16 - table_length / 2, 0, 1.0),
+            # "bins": (-0.5, -0.1, 0),
+            # "empty": (-0.6, 0, 0),
+            # "table": lambda table_length: (-0.16 - table_length / 2, 0, 1.0),
+            "bins": (-0.30, -0.1, 0.97),
+            "empty": (-0.29, 0, 0.97),
+            "table": lambda table_length: (-0.15 - table_length / 2, 0, 0.97),
         }
 
     @property
@@ -166,13 +106,15 @@ class Demo(ManipulatorModel):
     @property
     def default_controller_config(self):
         # Copied from panda.py
-        return {"right": "default_panda"}
+        return {"right": "default_gr1"}
 
     @property
     def init_qpos(self):
         # Copied from panda.py
-        # return np.array([0, np.pi / 16.0, 0.00, -np.pi / 2.0 - np.pi / 3.0, 0.00, np.pi - 0.2, np.pi / 4])
-        return np.array([0.0]*6)
+        right_arm_init = np.array([0.0]*6)
+        # right_arm_init = np.array([0.0, -0.1, 0.0, -np.pi/2, np.pi, 0.0])
+        # right_arm_init = np.array([0.0, -0.1, 0.0, -1.57, 0.0, 0.0])
+        return right_arm_init
 
     # @property
     # def key_map(self):
