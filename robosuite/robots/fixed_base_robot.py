f1184452867b535ad13047c54160dc217b9140ed
Abhiram824 <abhisuhaas1@gmail.com>
2025-05-16 16:26:12 -0500

    make all envs in robosuite seedable

--- robosuite/robots/fixed_base_robot.py
+++ robosuite/robots/fixed_base_robot.py
@@ -67,7 +67,7 @@ class FixedBaseRobot(Robot):
         # First, run the superclass method to load the relevant model
         super().load_model()
 
-    def reset(self, deterministic=False):
+    def reset(self, deterministic=False, rng=None):
         """
         Sets initial pose of arm and grippers. Overrides gripper joint configuration if we're using a
         deterministic reset (e.g.: hard reset from xml file)
@@ -76,7 +76,7 @@ class FixedBaseRobot(Robot):
             deterministic (bool): If true, will not randomize initializations within the sim
         """
         # First, run the superclass method to reset the position and controller
-        super().reset(deterministic)
+        super().reset(deterministic, rng=rng)
 
     def _create_base_sensors(self, modality):
         """
